84f46f39f9297948775414c5b63ddd716315ec95
J. Taylor O'Connor <taylor@singlemusic.com>
2020-05-22 13:37:09 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tn-covid-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tennessee-covid';
+}
